45af0f05a49d7298d11fb64d0feb97ae8facc59c
Jmancuso42 <91137264+Jmancuso42@users.noreply.github.com>
2022-08-11 20:05:41 -0400

    Add files via upload

---
+++ monthly-venmo-main/accessToken.py
@@ -0,0 +1,31 @@
+from venmo_api import Client
+import venmo_api
+import os
+from dotenv import load_dotenv
+from utils import get_env, env_vars, get_month, Venmo, Telegram
+
+#access_token = Client.get_access_token(username='jmancuso142@gmail.com',
+ #                                       password='#uclid3an')
+
+load_dotenv()
+#os.getenv(key=venmo_api.)
+at = get_env("JTOKEN")
+print(at)
+#print("My Token:", access_token)
+
+venmo = Venmo(at)
+#cjtoken = venmo_api.get_user_id(user="CJZesty")
+cjToken = venmo.get_user_id_by_username("CJZesty")
+print("Cj token:", cjToken)
+
+e_token = venmo.get_user_id_by_username("Imgrana1")
+print("Een token:", e_token)
+
+m_token= venmo.get_user_id_by_username("Mitchell-Schaffer-2")
+print("mitch token:", m_token)
+
+mi_token = venmo.get_user_id_by_username("Michael-Ciccarone-1")
+print("mikelToken:", mi_token)
+
+r_token = venmo.get_user_id_by_username("Ryan-Lehmann-6")
+print("ryan token",r_token)
---
+++ monthly-venmo-main/init.py
@@ -0,0 +1,74 @@
+from venmo_api import Client
+from dotenv import load_dotenv
+from notifiers import get_notifier
+from datetime import datetime
+
+from utils import get_env, env_vars, get_month, Venmo, Telegram
+
+def main(now):
+  """
+  The main function which initiates the script.
+  """
+
+  load_dotenv()  # take environment variables from .env.
+  actualVars = []
+  for var in env_vars:
+    actualVars.append(get_env(var))
+
+  access_token, chat_id, bot_token, e_friend_id, c_friend_id, m_friend_id, r_friend_id, mi_friend_id = actualVars
+
+  month = get_month(now)
+  venmo = Venmo(access_token)
+  telegram = Telegram(bot_token, chat_id)
+
+  friends =[
+    {
+      "name": "Een",
+      "id": e_friend_id,
+    },
+    {
+      "name": "CJ",
+      "id": c_friend_id,
+    },
+    {
+      "name": "Mike",
+      "id": m_friend_id,
+    },
+
+    {
+      "name": "Mitch",
+      "id": mi_friend_id,
+    },
+
+    {
+      "name": "Ryan",
+      "id": r_friend_id,
+    },
+
+  ]
+
+  successfulRequests = []
+  expectedRequests = len(friends)
+
+  for friend in friends:
+    name = friend["name"]
+    id = friend["id"]
+    description = "That time of month again to shoot youtube ads dead, for the month of " + month + "— Sent by Joe's Robo-wage-slave"
+    amount = 3.60
+    message = f"""Good news
+
+I have successfully requested money from {name}.
+
+— Lil Joe
+    """
+    success = venmo.request_money(id, amount, description, telegram.send_message(message))
+    if success:
+      successfulRequests.append(success)
+
+  if len(successfulRequests) == expectedRequests:
+    print("✅ Ran script successfully and sent " + str(expectedRequests) + " Venmo requests.")
+  else:
+    print("❌ Something went wrong. Only sent " + str(len(successfulRequests)) + "/" + str(expectedRequests) + " venmo requests.")
+
+now = datetime.now()
+main(now)
